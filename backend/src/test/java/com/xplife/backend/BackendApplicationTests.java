11f1943ba924e7a3bbf1d20ac37e52788497e8d2
Devin Crowley <devinctrl@gmail.com>
2025-07-07 19:15:51 -0400

    Scaffold Spring Boot project with Maven and empty structure

---
+++ backend/src/test/java/com/xplife/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xplife.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
